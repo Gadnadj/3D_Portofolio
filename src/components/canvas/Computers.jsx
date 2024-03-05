f5f2423f8d58e29eca7c47c8548b188e3b7c813f
Gad Nadjar <gadnadjar@gmail.com>
2024-03-05 12:18:29 +0200

    add 3D computer fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,8 +8,7 @@ const Computers = () => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <pointLight intensity={1} />
+      <hemisphereLight intensity={3.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -18,10 +17,11 @@ const Computers = () => {
         castShadow
         shadow-mapSize={1024}
       />
+      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
         scale={0.75}
-        position={[0, -3.25 - 1.5]}
+        position={[0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
