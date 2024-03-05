4f0c9573d8eb75288cce855e9560e552a456109b
Gad Nadjar <gadnadjar@gmail.com>
2024-03-05 13:20:20 +0200

    done about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,51 +1,60 @@
 import React from 'react';
+import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
+
 import { styles } from '../styles';
 import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
-import { Tilt } from 'react-tilt';
 import { SectionWrapper } from '../hoc';
+import { fadeIn, textVariant } from '../utils/motion';
 
-const ServiceCard = ({ index, title, icon }) => {
-  return (
-    <Tilt className='xs:w-[250px] w-full'>
-      <motion.div
-        variants={fadeIn('right', 'spring', 0.5 * index, 0.74)}
-        className='w-full green-pink-gradient p-1 rounded-[20px] shadow-card'
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
       >
-        <div option={{ max: 45, scale: 1, speed: 450 }}>
-          <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
-            <img src={icon} alt='title' className='w-16 h-16 object-contain ' />
-            <h3 className='text-white text-[20px] font-bold text-center'>
-              {title}
-            </h3>
-          </div>
-        </div>
-      </motion.div>
-    </Tilt>
-  );
-};
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
-        className='mt-4 text-secondary text-[17px], max-w-8xl leading-[30px]'
+        className='mt-4 text-secondary text-[17px] max-w-7xl leading-[30px]'
       >
-        I'm a front-end developer with a passion for creating beautiful and
-        functional user interfaces. I have a strong foundation in JavaScript,
-        React, and CSS, and I'm always looking to learn new technologies and
-        improve my skills. I'm also a big fan of design and enjoy working
-        closely with designers to bring their ideas to life.
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10 '>
+      <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
