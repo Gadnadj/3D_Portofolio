bc238377e3d025c4095f91d0552da430c1a94052
Gad Nadjar <gadnadjar@gmail.com>
2024-03-05 13:03:02 +0200

    add about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { Tilt } from 'react-tilt';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn('right', 'spring', 0.5 * index, 0.74)}
+        className='w-full green-pink-gradient p-1 rounded-[20px] shadow-card'
+      >
+        <div option={{ max: 45, scale: 1, speed: 450 }}>
+          <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+            <img src={icon} alt='title' className='w-16 h-16 object-contain ' />
+            <h3></h3>
+          </div>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className='mt-4 text-secondary text-[17px], max-w-8xl leading-[30px]'
+      >
+        I'm a front-end developer with a passion for creating beautiful and
+        functional user interfaces. I have a strong foundation in JavaScript,
+        React, and CSS, and I'm always looking to learn new technologies and
+        improve my skills. I'm also a big fan of design and enjoy working
+        closely with designers to bring their ideas to life.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10 '>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
