76d0e9682f509b5620e5437ae24e79fc315d8bf6
Gad Nadjar <gadnadjar@gmail.com>
2024-03-05 14:58:47 +0200

    add content

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,19 +42,19 @@ export const navLinks = [
 
 const services = [
   {
-    title: 'Web Developer',
+    title: 'Front End Developer',
     icon: web,
   },
   {
-    title: 'React Native Developer',
+    title: 'React Developer',
     icon: mobile,
   },
   {
-    title: 'Backend Developer',
+    title: 'Responsive Web Design',
     icon: backend,
   },
   {
-    title: 'Content Creator',
+    title: 'UI/UX Designer',
     icon: creator,
   },
 ];
