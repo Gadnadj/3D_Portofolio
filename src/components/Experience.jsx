85e0828c8358649e5853e10753bcbbc92e3a9e13
Gad Nadjar <gadnadjar@gmail.com>
2024-03-05 14:45:21 +0200

    fix bugs

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from 'react-vertical-timeline-component';
 import { motion } from 'framer-motion';
+
 import 'react-vertical-timeline-component/style.min.css';
 
 import { styles } from '../styles';
@@ -13,7 +15,10 @@ import { textVariant } from '../utils/motion';
 const ExperienceCard = ({ experience }) => {
   return (
     <VerticalTimelineElement
-      contentStyle={{ background: '#1d1836', color: '#fff' }}
+      contentStyle={{
+        background: '#1d1836',
+        color: '#fff',
+      }}
       contentArrowStyle={{ borderRight: '7px solid  #232631' }}
       date={experience.date}
       iconStyle={{ background: experience.iconBg }}
@@ -39,8 +44,12 @@ const ExperienceCard = ({ experience }) => {
 
       <ul className='mt-5 list-disc ml-5 space-y-2'>
         {experience.points.map((point, index) => (
-          <li key={`experience-point-${index}`}
-          className='text-white '></li>
+          <li
+            key={`experience-point-${index}`}
+            className='text-white-100 text-[14px] pl-1 tracking-wider'
+          >
+            {point}
+          </li>
         ))}
       </ul>
     </VerticalTimelineElement>
@@ -51,14 +60,21 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>What i have done so far</p>
-        <h2 className={styles.sectionHeadText}>Experience.</h2>
+        <p className={`${styles.sectionSubText} text-center`}>
+          What I have done so far
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Work Experience.
+        </h2>
       </motion.div>
 
       <div className='mt-20 flex flex-col'>
         <VerticalTimeline>
           {experiences.map((experience, index) => (
-            <ExperienceCard key={index} experience={experience} />
+            <ExperienceCard
+              key={`experience-${index}`}
+              experience={experience}
+            />
           ))}
         </VerticalTimeline>
       </div>
@@ -66,4 +82,4 @@ const Experience = () => {
   );
 };
 
-export default SectionWrapper(Experience, 'word');
+export default SectionWrapper(Experience, 'work');
