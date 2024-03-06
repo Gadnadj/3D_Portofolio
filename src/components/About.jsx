6015f5c6a2dba68279fcd64defd88f93bdf7f112
Gad Nadjar <gadnadjar@gmail.com>
2024-03-06 17:51:44 +0200

    add styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,13 +47,13 @@ const About = () => {
         variants={fadeIn('', '', 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a 4th year Student at Sami Shamoon college of engineering, I'm a
-        front-end developer with a passion for creating beautiful and functional
-        user interfaces. I have a strong foundation in web development, and I'm
-        always looking to learn new technologies and tools. I'm a quick learner
-        and a problem-solver, and I'm always looking for new challenges and
-        opportunities to grow. I'm use react and tailwindcss to build web
-        applications and user interaces.
+        I'm a 4th year Software Engineering Student at Sami Shamoon College of
+        Engineering, I'm a front-end developer with a passion for creating
+        beautiful and functional user interfaces. I have a strong foundation in
+        web development, and I'm always looking to learn new technologies and
+        tools. I'm a quick learner and a problem-solver, and I'm always looking
+        for new challenges and opportunities to grow. I'm use react and
+        tailwindcss to build web applications and user interaces.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
