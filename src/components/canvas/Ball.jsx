a58bc0c7c76b84c5ae07afb99d23990ec48287c6
Gad Nadjar <gadnadjar@gmail.com>
2024-03-06 09:36:49 +0200

    add technologies

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
-const Ball = () => {
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+  return <div>Ball</div>;
+};
+
+const BallCanvas = ({ icon }) => {
   return (
-    <div>Ball</div>
-  )
-}
+    <Canvas
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
 
-export default Ball
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default Ball;
